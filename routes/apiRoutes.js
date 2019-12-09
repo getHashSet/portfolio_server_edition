bad1b8a89d05da5706b46133af6f893611e1eb66
Matthew <matthew_carpenter@icloud.com>
2019-12-08 20:47:50 -0700

    changed DB name on all routes.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,22 +3,22 @@ var db = require("../models");
 module.exports = function(app) {
   // Get all examples
   app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+    db.Example.findAll({}).then(function(portfolio_dbs) {
+      res.json(portfolio_dbs);
     });
   });
 
   // Create a new example
   app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+    db.Example.create(req.body).then(function(portfolio_db) {
+      res.json(portfolio_db);
     });
   });
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+    db.Example.destroy({ where: { id: req.params.id } }).then(function(portfolio_db) {
+      res.json(portfolio_db);
     });
   });
 };
