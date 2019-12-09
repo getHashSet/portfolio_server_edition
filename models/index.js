00829d1c9fe0eff1c1f8e90dc983d9d760b34d47
Matthew <matthew_carpenter@icloud.com>
2019-12-08 22:04:04 -0700

    meh

--- models/index.js
+++ models/index.js
@@ -5,12 +5,13 @@ var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || "development";
+//var config = require(__dirname + "/../config/config.json")[env];
+
 var config = require(__dirname + "/../config/config.json")[process.env.NODE_ENV];
-//var config = require(__dirname + "/../config/config.json")["development"];
 var db = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(config[process.env.NODE_ENV]);
+  var sequelize = new Sequelize(process.env[config.use_env_variable], {"dialect": "mysql"});
 } else {
   var sequelize = new Sequelize(
     config.database,
