e9373ed27ab339e793689b33aa448187187b55c4
Matthew <matthew_carpenter@icloud.com>
2019-12-08 22:16:24 -0700

    reset

--- models/index.js
+++ models/index.js
@@ -5,9 +5,9 @@ var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || "development";
-//var config = require(__dirname + "/../config/config.json")[env];
+var config = require(__dirname + "/../config/config.json")[env];
 
-var config = require(__dirname + "/../config/config.json")[process.env.NODE_ENV];
+//var config = require(__dirname + "/../config/config.json")[process.env.NODE_ENV];
 var db = {};
 
 if (config.use_env_variable) {
